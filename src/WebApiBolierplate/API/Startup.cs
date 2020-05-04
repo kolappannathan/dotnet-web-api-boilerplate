d4b4694cb76f9c67d820c8c15904e969f9b6df71
Kolappan Nathan <kolappannathan@users.noreply.github.com>
2020-05-04 19:54:10 +0530

    .Net core 3.0 changes

--- src/WebApiBolierplate/API/Startup.cs
+++ src/WebApiBolierplate/API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
@@ -67,7 +68,7 @@ namespace API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
