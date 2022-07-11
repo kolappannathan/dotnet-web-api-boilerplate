e9eadbe13bafb973f3f2a5eb3d676c6c5c0532f0
Kolappan Nathan <kolappannathan@users.noreply.github.com>
2022-07-11 22:01:05 +0530

    Fixes #82

--- src/WebApiBolierplate/API/Program.cs
+++ src/WebApiBolierplate/API/Program.cs
@@ -1,20 +1,69 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using API.Helpers;
+using Business.Lib.Core;
+using Core.Constants;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
-namespace API
-{
-    public class Program
-    {
-        public static void Main(string[] args)
+var builder = WebApplication.CreateBuilder(args);
+
+// To prevent .NET and server info from being added to header if Kestrel is used
+builder.WebHost.ConfigureKestrel(serverOptions => {
+    serverOptions.AddServerHeader = false;
+});
+
+#region Configuring Services
+
+var startupLib = new StarupLib();
+startupLib.LoadConfig(builder.Configuration);
+
+builder.Services.AddControllers();
+
+#region [Validation Handling]
+
+// To handle model validation errors and change it to 
+// Ref: https://stackoverflow.com/a/51159755/5407188
+
+var validationHelper = new ValidationHelper();
+
+builder.Services.Configure<ApiBehaviorOptions>(o => {
+    o.InvalidModelStateResponseFactory = actionContext => validationHelper.GetDataValidationError(actionContext.ModelState);
+});
+
+#endregion [Validation Handling]
+
+#region [Authentication]
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => {
+        options.TokenValidationParameters = new TokenValidationParameters
         {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = Config.JWT.Issuer,
+            ValidAudience = Config.JWT.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Config.JWT.Key))
+        };
+    });
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .ConfigureKestrel(serverOptions => {
-                    serverOptions.AddServerHeader = false;
-                })
-                .UseStartup<Startup>();
-    }
+#endregion [Authentication]
+
+#endregion Configuring Services
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
