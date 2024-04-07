7dcb789d00d69a5a3e3d401be1cf47361cb9f8e5
Kolappan Nathan <kolappannathan@users.noreply.github.com>
2024-04-07 23:07:06 +0530

    Fixes #125

---
+++ src/WebApiBolierplate/Core.Test/Utilities/JwtUtilsTest.cs
@@ -0,0 +1,56 @@
+﻿using Core.Lib.Utilities.Interfaces;
+using Core.Lib.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Design;
+using Core.Constants;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Core.Test.Utilities;
+
+[TestClass]
+public sealed class JwtUtilsTest
+{
+    private const string SecurityKey = "This is a test key for JWT token builder";
+    private const string Audience = "Test Audience";
+    private readonly IJwtUtils _jwtUtils;
+
+    public JwtUtilsTest()
+    {
+        _jwtUtils = new JwtUtils();
+    }
+
+    [TestMethod]
+    public void TestJwtTokenBuilder()
+    {
+        _jwtUtils
+            .AddSecurityKey(SecurityKey)
+            .AddIssuer(nameof(JwtUtilsTest))
+            .AddAudience(Audience)
+            .AddExpiry(10)
+            .AddRole(Roles.User)
+            .AddName("Eddard Stark")
+            .AddUserId("STARK002")
+            .AddCompanyId("GOT");
+
+        var token = _jwtUtils.GenerateToken();
+        Assert.IsNotNull(token);
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        tokenHandler.ValidateToken(token, new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecurityKey)),
+            ValidIssuer = nameof(JwtUtilsTest),
+            ValidAudience = Audience
+        }, out var validatedToken);
+        var jwtToken = (JwtSecurityToken)validatedToken;
+        Assert.AreEqual(token, jwtToken.RawData);
+    }
+
+}
